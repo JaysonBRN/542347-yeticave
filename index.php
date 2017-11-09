884d35f9336db11e8b2ef3efa8eceadc44f201fa
JaysonBRN <aksiburn@yahoo.com>
2017-11-10 02:37:54 +0500

    Только арифметика и floor

--- index.php
+++ index.php
@@ -7,17 +7,16 @@ $user_avatar = 'img/user.jpg';
 // устанавливаем часовой пояс в Московское время
 date_default_timezone_set('Europe/Moscow');
 
-// записать в эту переменную оставшееся время в этом формате (ЧЧ:ММ)
-$lot_time_remaining = "00:00";
-
 // временная метка для полночи следующего дня
 $tomorrow = strtotime('tomorrow midnight');
 
 // временная метка для настоящего времени
 $now = strtotime('now');
 
-// далее нужно вычислить оставшееся время до начала следующих суток и записать его в переменную $lot_time_remaining
-$lot_time_remaining = gmdate("H:i", $tomorrow - $now);
+$hours = ($tomorrow - $now) % 3600 ;
+$hoursFull = floor(($tomorrow - $now) / 3600) ;
+$minutes = floor($hours / 60) ;
+$lot_time_remaining = $hoursFull . ':' . $minutes ;
 ?>
 <!DOCTYPE html>
 <html lang="ru">
