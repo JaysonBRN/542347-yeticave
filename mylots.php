e984a6812ba14dddf1cac7ec92ebb6be986d35f0
Zakirov Igor <zakirovir6@gmail.com>
2017-12-09 01:25:28 +0500

    edits

--- mylots.php
+++ mylots.php
@@ -1,6 +1,18 @@
 <?php
 require_once ('app/init.php');
 $lotbet = getBetsByUserId($autorizedUser['id']);
+
+foreach ( $lotbet as $bet )
+{
+    $lot = getLotById( $bet['lotid'], $lots );
+    $category = getCategoryById( $lot['category'], $categories );
+
+    $bet['lot-img'] = $lot['lot-img'];
+    $bet['lot-name'] = $lot['lot-name'];
+    $bet['lot-id'] = $lot['id'];
+    $bet['category-name'] = $category['name'];
+}
+
 $pagecontent = include_template('templates/mylots.php', ['lotbet' => $lotbet]);
 $layoutcontent = include_template('templates/layout.php', ['content' => $pagecontent, 'title' => 'yeticave - Главная', 'autorizedUser' => $autorizedUser]);
 echo $layoutcontent;
--- templates/mylots.php
+++ templates/mylots.php
@@ -1,25 +1,25 @@
 <section class="rates container">
     <h2>Мои ставки</h2>
     <table class="rates__list">
-        <?php foreach ($lotbet as $key => $value): ?>
+        <?php foreach ( $lotbet as $key => $bet): ?>
         <tr class="rates__item">
             <td class="rates__info">
                 <div class="rates__img">
-                    <img src="img/rate1.jpg" width="54" height="40" alt="Сноуборд">
+                    <img src="<?= $bet['lot-img'] ?>" width="54" height="40" alt="Сноуборд">
                 </div>
-                <h3 class="rates__title"><a href="lot.html"><?=$lot[$value['lot-name']];?></a></h3>
+                <h3 class="rates__title"><a href="/lot.php?id=<?= $bet['lot-id'] ?>"><?= $bet['lot-name']?></a></h3>
             </td>
             <td class="rates__category">
-                Доски и лыжи<?=$value[''] ?>
+                <?= $bet['category-name'] ?>
             </td>
             <td class="rates__timer">
                 <div class="timer timer--finishing">07:13:34</div>
             </td>
             <td class="rates__price">
-                10 999 р<?=$value['cost']; ?>
+                10 999 р<?=$bet['cost']; ?>
             </td>
             <td class="rates__time">
-                <?=timeform($value['time']); ?>
+                <?=timeform($bet['time']); ?>
             </td>
         </tr>
         <tr class="rates__item">
